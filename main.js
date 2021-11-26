88d4c5999b701dfad6f50ec52ca3681727504373
TPHL1611 <tranphanhailong89@gmail.com>
2021-11-26 22:33:02 +0700

    resposive

--- main.js
+++ main.js
@@ -2,20 +2,24 @@ function openNav() {
     const mobileMenu = document.querySelector('.nav-menu');
     mobileMenu.onclick = function() {
         document.querySelector(".nav_content").style.width = '30rem';
-        document.querySelector(".nav_content").style.left = '0';
+        document.querySelector(".nav_content").style.padding = '0 4.8rem';
+        document.querySelector(".nav_content").style.left = '1.6rem';
     }
 }
 
 function closeNav() {
-    const main = document.getElementById('main');
-    main.onclick = function() {
+    const closeMenu = document.querySelector('.nav_content--close')
+    closeMenu.onclick = function() {
         document.querySelector(".nav_content").style.width = '0';
+        document.querySelector(".nav_content").style.padding = '0';
         document.querySelector(".nav_content").style.left = '-10rem';
-        console.log(this)
     }
+
 }
 
 function start() {
     openNav();
     closeNav();
 }
+// const navContent = document.querySelector('.nav_content')
+// console.log([navContent])
