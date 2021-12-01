309c05e8fea4426192d84fcef262af0643ed3aad
TPHL1611 <tranphanhailong89@gmail.com>
2021-12-01 21:55:55 +0700

    responsive

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 function openNav() {
-    const mobileMenu = document.querySelector('.nav-menu');
+    const mobileMenu = document.querySelector('.nav__item-menu');
     mobileMenu.onclick = function() {
-        document.querySelector(".nav_content").style.width = '30rem';
-        document.querySelector(".nav_content").style.padding = '0 4.8rem';
-        document.querySelector(".nav_content").style.left = '1.6rem';
+        document.querySelector('.overlay').style.width ='200%';
+        document.querySelector('.overlay').style.height ='200%';
+
+        document.querySelector(".menu-mobile").style.width = '30rem';
+        document.querySelector(".menu-mobile").style.padding = '0 4.8rem';
+        document.querySelector(".menu-mobile").style.left = '-6%';
     }
 }
 
 function closeNav() {
-    const closeMenu = document.querySelector('.nav_content--close')
+    const closeMenu = document.querySelector('.overlay')
     closeMenu.onclick = function() {
-        document.querySelector(".nav_content").style.width = '0';
-        document.querySelector(".nav_content").style.padding = '0';
-        document.querySelector(".nav_content").style.left = '-10rem';
+        document.querySelector('.overlay').style.width ='0';
+        document.querySelector(".menu-mobile").style.width = '0';
+        document.querySelector(".menu-mobile").style.left = '-65%';
     }
 
+}
+
+function openMenu() {
+    const menuProducts = document.querySelectorAll(".product-details__menu--item")
+    const descProducts = document.querySelectorAll(".product-details__wrap")
+
+    menuProducts.forEach(function(menuProduct, index) {
+        const descProduct = descProducts[index]
+
+        menuProduct.onclick = function() {
+
+            console.log(this);
+            document.querySelector(".product-details__menu--item.active").classList.remove('active');
+            document.querySelector(".product-details__wrap.active").classList.remove('active');
+            
+            this.classList.add('active');
+            descProduct.classList.add('active');
+        }
+    })
 }
 
 function start() {
     openNav();
     closeNav();
+    openMenu();
 }
 // const navContent = document.querySelector('.nav_content')
 // console.log([navContent])
